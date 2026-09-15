c7605b1c9fd8d1c45fe82be5122895c6a95f3ada
lobolos98 <charliejonesart@gmail.com>
2026-09-15 17:56:47 -0400

    Simplify Astro configuration for static Cloudflare deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   output: "static",
-  adapter: cloudflare(),
   site: "https://atectarchitecture.com"
 });
